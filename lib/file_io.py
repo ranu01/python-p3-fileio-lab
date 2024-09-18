2ce0c23202bf005cd97e28cd3e99b2fb4da4bdc0
Lewis <lewiskaranu0@gmail.com>
2024-09-18 12:37:03 +0300

    lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,28 @@
+# lib/file_io.py
+
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    
+    file_path = Path(f"{file_name}.txt")
+    
+    with open(file_path, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+   
+    file_path = Path(f"{file_name}.txt")
+   
+    with open(file_path, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+  
+    file_path = Path(f"{file_name}.txt")
+    
+    
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return None
